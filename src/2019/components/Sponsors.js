c63fcbe8dd196ff120db01972aaa03543880b3be
Lisa Luo <llluo@ucsd.edu>
2019-10-16 20:45:50 -0700

    updated with sponsors and partners

--- src/2019/components/Sponsors.js
+++ src/2019/components/Sponsors.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
-import {mediaBreakpointDown} from '../../breakpoints';
 
 import {
     Page,
@@ -10,7 +9,7 @@ import {
 } from '../styles';
 
 import {SponsorsConstants} from '../constants';
-import {sponsors} from '../data/Sponsors.js'
+import {tierOneSponsor, tierTwoSponsor, tierThreeSponsor, tierFourSponsor, tierOnePartner, tierTwoPartner} from '../data/Sponsors.js'
 
 const SponsorsSection = styled(Page)`
     background: url('/sea-animals/silhouette.svg'), ${SponsorsConstants.background};
@@ -24,8 +23,7 @@ const SponsorContainer = styled.a`
 `
 
 const SponsorContent = styled(SectionContent)`
-    padding-top: 1rem;
-    background: rgba(255, 255, 255, 0.2);
+    background: rgba(255, 255, 255, 0.5);
     border-radius: 45px;
 `
 
@@ -33,55 +31,21 @@ const AnglerFish = styled.img`
     width: 60%;
 `
 
-const SmallImg = styled.img`
-    width: 70%;
-
-    ${mediaBreakpointDown('md', `
-        width: 40%;
-    `)}
-`
-
-const MediumImg = styled.img`
-    width: 90%;
-
-    ${mediaBreakpointDown('md', `
-        width: 50%;
-    `)}
-`
-
-const LargeImg = styled.img`
-    width: 110%;
-`
-
 function Sponsor(props) {
     const col = props.col || 'col-md-4 col-lg-4';
     console.log(props);
 
-    if (props.size === "small") {
-        return (
-            <SponsorContainer className={`${col} my-5 d-block`} href={props.link} target="_blank">
-                <SmallImg src={props.logo} className={`img-fluid d-block m-auto  ${props.className}`} alt="" />
-            </SponsorContainer>
-        )
-    } else  if (props.size === "medium") {
-        return (
-            <SponsorContainer className={`${col} my-5 d-block`} href={props.link} target="_blank">
-                <MediumImg src={props.logo} className={`img-fluid d-block m-auto  ${props.className}`} alt="" />
-            </SponsorContainer>
-        )
-    } else {
-        return (
-            <SponsorContainer className={`${col} my-5 d-block`} href={props.link} target="_blank">
-                <LargeImg src={props.logo} className={`img-fluid d-block m-auto  ${props.className}`} alt="" width="110%"/>
-            </SponsorContainer>
-        )
-    }
+    return (
+        <SponsorContainer className={`${col} my-5 d-block`} href={props.link} target="_blank">
+            <img src={props.logo} className={`img-fluid d-block m-auto  ${props.className}`} alt="" width={`${props.width}%`} />
+        </SponsorContainer>
+    )
 }
 
 class Sponsors extends Component {
     renderSponsors(currSponsors) {
-        const COL_LENS = ['col-md-9 col-lg-9', 'col-md-4 col-lg-4', 'col-md-3 col-md-3'];
-        return currSponsors.map(p => <Sponsor {...p} col={COL_LENS[p.tier]}/>)
+        const COL_LENS = ['col-md-9 col-lg-9', 'col-md-4 col-lg-4', 'col-md-3 col-md-3', 'col-sm-3 col-sm-3'];
+        return currSponsors.map(p => <Sponsor {...p} col={COL_LENS[p.tier]} />)
     }
 
     render() {
@@ -90,23 +54,35 @@ class Sponsors extends Component {
                 <Container className="d-flex mx-auto h-100">
                     <SectionContent>
                         <div className="container-fluid">
+                            
                             <div className="row justify-content-center">
                                 <SectionHeader>Sponsors</SectionHeader>
                             </div>
-                            {/*<SponsorContent>
-                            <div className="row align-items-center justify-content-center">
-                                {this.renderSponsors(sponsors)}
-                            </div>
-                            {/*<div className="row justify-content-center">
+                            <SponsorContent>
+                                <div className="row align-items-center justify-content-center">
+                                    {this.renderSponsors(tierOneSponsor)}
+                                </div>
+                                <div className="row align-items-center justify-content-center">
+                                    {this.renderSponsors(tierTwoSponsor)}
+                                </div>
+                                <div className="row align-items-center justify-content-center">
+                                    {this.renderSponsors(tierThreeSponsor)}
+                                </div>
+                                <div className="row align-items-center justify-content-center">
+                                    {this.renderSponsors(tierFourSponsor)}
+                                </div>
+                            </SponsorContent>
+
+                            <div className="row justify-content-center">
                                 <SectionHeader>Partners</SectionHeader>
                             </div>
-                            <div className="row align-items-center justify-content-center">
-                                {this.renderSponsors(partners)}
-                            </div>}
-                            </SponsorContent>*/}
-
-                            <SponsorContent className="row align-items-center justify-content-around">
-                                {this.renderSponsors(sponsors)}
+                            <SponsorContent>
+                                <div className="row align-items-center justify-content-center">
+                                    {this.renderSponsors(tierOnePartner)}
+                                </div>
+                                <div className="row align-items-center justify-content-center">
+                                    {this.renderSponsors(tierTwoPartner)}
+                                </div>
                             </SponsorContent>
 
                             <div className="row">
--- src/2019/data/Sponsors.js
+++ src/2019/data/Sponsors.js
@@ -1,48 +1,131 @@
-const pastSponsors = [
-
-]
-
 const tierOneSponsor = [
     {
-        name: 'Northrop Grumman',
-        logo: '/sponsors/northrop-grumman.svg',
-        link: 'https://www.northropgrumman.com/Pages/default.aspx',
-        size: 'normal'
-    },
-    {
-        name: 'Activbody',
-        logo: '/sponsors/activbody.svg',
-        link: 'https://activbody.com/',
-        size: 'normal'
+        name: 'Naval Information Warfare Center - Pacific',
+        logo: '/sponsors/niwc.svg',
+        link: 'https://www.public.navy.mil/navwar/NIWC-Pacific/Pages/default.aspx',
+        width: '50'
     }
 ].map(x => ({...x, tier: 0}));
 
 const tierTwoSponsor = [
     {
+        name: 'Northrop Grumman',
+        logo: '/sponsors/northrop.svg',
+        link: 'https://www.northropgrumman.com/Pages/default.aspx',
+        width: '100'
+    },
+    {
+        name: 'Activbody',
+        logo: '/sponsors/activbody.svg',
+        link: 'https://activbody.com/',
+        size: '100'
+    },
+    {
+        name: 'UCSD CSE',
+        logo: '/sponsors/jsoe-cse.svg',
+        link: 'https://cse.ucsd.edu/',
+        size: '100'
+    }
+].map(x => ({...x, tier: 1}));
+
+const tierThreeSponsor = [
+    {
         name: 'Amazon Web Services',
         logo: '/sponsors/aws.svg',
         link: 'https://aws.amazon.com/',
-        size: 'small'
+        width: '60'
     }, 
     {
         name: 'Surcle',
         logo: '/sponsors/surcle.svg',
         link: 'https://surcle.io',
-        size: 'medium'
+        width: '90'
+    },
+    {
+        name: 'Costar',
+        logo: '/sponsors/costar.svg',
+        link: 'https://www.costar.com/',
+        width: '90'
     }
-].map(x => ({...x, tier: 1}));
-
-const sponsors =  [
-    ...tierOneSponsor,
-    ...tierTwoSponsor
-]
-
-const partners = [
-
 ].map(x => ({...x, tier: 2}));
 
+const tierFourSponsor = [
+    {
+        name: 'Microsoft',
+        logo: '/sponsors/microsoft.svg',
+        link: 'https://www.microsoft.com/en-us/',
+        width: '70'
+    },
+    {
+        name: 'UCSD ECE',
+        logo: '/sponsors/jsoe-ece.svg',
+        link: 'http://ece.ucsd.edu/',
+        width: '70'
+    },
+    {
+        name: 'Wolfram',
+        logo: '/sponsors/wolfram.svg',
+        link: 'https://www.wolfram.com/',
+        width: '70'
+    }
+].map(x => ({...x, tier: 2}));
+
+const tierOnePartner = [
+    {
+        name: 'ACM',
+        logo: '/partners/acm.svg',
+        width: '60'
+    },
+    {
+        name: 'BMES',
+        logo: '/partners/bmes.svg',
+        width: '60'
+    },
+    {
+        name: 'CSES',
+        logo: '/partners/cses.svg',
+        width: '60'
+    },
+    {
+        name: 'DS3',
+        logo: '/partners/ds3.svg',
+        width: '60'
+    }
+].map(x => ({...x, tier: 3}));
+
+const tierTwoPartner = [
+    {
+        name: 'ESW',
+        logo: '/partners/esw.svg',
+        width: '50'
+    },
+    {
+        name: 'IEEE',
+        logo: '/partners/ieee.svg',
+        width: '50'
+    },
+    {
+        name: 'SWE',
+        logo: '/partners/swe.svg',
+        width: '50'
+    },
+    {
+        name: 'Triton XR',
+        logo: '/partners/triton-xr.svg',
+        width: '30'
+    },
+    {
+        name: 'TSE',
+        logo: '/partners/tse.svg',
+        width: '25'
+    }
+].map(x => ({...x, tier: 3}));
+
 export {
-    pastSponsors,
-    sponsors,
-    partners
+    tierOneSponsor,
+    tierTwoSponsor,
+    tierThreeSponsor,
+    tierFourSponsor,
+    tierOnePartner,
+    tierTwoPartner
 }
